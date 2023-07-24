70b81f3bb80ed5e6e0b36a2a0f11a1b4afec4738
Ahmed Musawir <ahmed.musawir@gmail.com>
2023-07-24 13:43:30 +0800

    24July2023 - Cleanup and refactors

--- src/services/contactService.ts
+++
@@ -1,6 +0,0 @@
-import { Contact, ContactSingle } from "../entities";
-import APIClient from "../services/apiClientJsonSrv";
-
-export const singleContactService = new APIClient<Contact>("/contacts");
-
-export default new APIClient<Contact>("/contacts");
--- src/services/userService.ts
+++
@@ -1,4 +0,0 @@
-import { User } from "../entities";
-import APIClient from "../services/apiClient";
-
-export default new APIClient<User>("/v1/users");
